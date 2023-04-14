87300cf9f1237c3fc96e384a69a563289f3aa1ba
nikhil <nikhillilwani999@gmail.com>
2023-04-14 16:13:08 +0530

    changs

--- client/src/Service/api.js
+++ client/src/Service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
  export const getNews = async () => {
-    const URL = "mongodb://localhost:8000";
+    const URL = "http://localhost:8000";
     try {
         return await axios.get(`${URL}/news`)
     } catch (error) {
