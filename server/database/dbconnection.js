6094eb8f7511e9f059b075e30baabe0836abea9e
nikhil <nikhillilwani999@gmail.com>
2023-04-14 16:23:24 +0530

    changs server

--- server/database/dbconnection.js
+++ server/database/dbconnection.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
-mongoose.connect(process.env.DB_URL).then(() => {
+mongoose.connect(process.env.MDB_URL).then(() => {
     console.log("Database connection successfull");
 }).catch((err) => {
     console.log(err);
