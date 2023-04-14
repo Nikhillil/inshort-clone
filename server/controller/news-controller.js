f5780a772066a5bbb682bc4f4e5af3f3c8ea5e4d
nikhil <nikhillilwani999@gmail.com>
2023-04-14 15:29:56 +0530

    changes

--- server/controller/news-controller.js
+++ server/controller/news-controller.js
@@ -2,7 +2,7 @@ const News = require("../model/news-schema.js");
 
 const getNews = async (req,res) => {
      try {
-        const newsdata = await News.find({});
+        const newsdata = await News.find({ });
          res.status(200).json(newsdata);
      } catch (error) {
         res.status(400).json({message: error.message});
